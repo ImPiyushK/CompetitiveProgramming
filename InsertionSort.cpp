5cfe5324bd1a9860b7c0caef4246104198e37551
Piyush <ultrapiyushkandwal@gmail.com>
2024-12-08 15:32:36 +0530

    Insertion Sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 void sort(vector<int> &arr, int n){
-    
+    for(int i = 1 ; i < n ; ++i){
+        int key = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 int main(){
