0153b67fe5a3e3840891c9da56cf90d3a6aa53da
Piyush <ultrapiyushkandwal@gmail.com>
2024-12-16 16:42:47 +0530

    heap

--- heap.cpp
+++ heap.cpp
@@ -80,3 +80,62 @@ int main(){
 
     return 0;
 }
+
+//Heap Without Class
+#include <bits/stdc++.h>
+using namespace std ;
+
+void heapify(vector<int>&arr, int n , int index) {
+
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    int largestNode = index;
+
+    if (left < n  && arr[left] < arr[largestNode])
+        largestNode = left ;
+
+    if (right < n && arr[right] < arr[largestNode])
+        largestNode = right  ;
+
+    if (largestNode != index) {
+        swap(arr[index], arr[largestNode]);
+        heapify(arr, n, largestNode);
+    }
+}
+void createHeap(vector<int>&arr, int n) {
+
+    int start = (n / 2) - 1 ;
+
+    for (int index = start ; index >= 0; index--) {
+        heapify(arr, n, index)  ;
+    }
+}
+int extractMin(vector<int>& arr, int* n){
+    if((*n) == 0)
+        return INT_MIN;
+    swap(arr[0], arr[(*n) - 1]);
+    (*n)--;
+    heapify(arr, (*n) - 1, 0);
+    return arr[(*n)];
+}
+void printHeap(vector<int>&arr, int n) {
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << "\n"  ;
+}
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    vector<int>arr = {17, 15, 8, 4, 6, 13, 10, 9, 8, 3, 1};
+    int n = arr.size();
+
+    createHeap(arr, n);
+    printHeap(arr, n);
+    cout << extractMin(arr, &n) << '\n';
+    printHeap(arr, n);
+}
