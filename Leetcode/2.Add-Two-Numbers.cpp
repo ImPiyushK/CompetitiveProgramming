880d7d7416a4345d32f1a690756bf8ee773b88a5
Piyush <ultrapiyushkandwal@gmail.com>
2024-12-17 16:29:56 +0530

    2

--- Leetcode/2.Add-Two-Numbers.cpp
+++ Leetcode/2.Add-Two-Numbers.cpp
@@ -1,5 +1,6 @@
 //1. total sum - sum of vector
 //2. xor
+//3.binary search
 class Solution {
 public:
     int missingNumber(vector<int>& nums) { 
@@ -13,3 +14,28 @@ public:
     }
 };
 
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans =0;
+        for(int i =1;i<=n;i++){
+            ans = ans ^ i;
+        }
+        for(int i =0;i<nums.size();i++){
+            ans= ans^nums[i];
+        }
+        return ans;
+    }
+};
+
+public int missingNumber(int[] nums) { //binary search
+    Arrays.sort(nums);
+    int left = 0, right = nums.length, mid= (left + right)/2;
+    while(left<right){
+        mid = (left + right)/2;
+        if(nums[mid]>mid) right = mid;
+        else left = mid+1;
+    }
+    return left;
+}
