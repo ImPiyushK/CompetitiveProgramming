5bb43d1c5e98ce37c38c7e62149da0f2bf620d50
Piyush <ultrapiyushkandwal@gmail.com>
2024-12-17 16:31:30 +0530

    2

--- Leetcode/2.Add-Two-Numbers.cpp
+++ Leetcode/2.Add-Two-Numbers.cpp
@@ -31,7 +31,7 @@ public:
 
 public int missingNumber(int[] nums) { //binary search
     Arrays.sort(nums);
-    int left = 0, right = nums.length, mid= (left + right)/2;
+    int left = 0, right = nums.size(), mid= (left + right)/2;
     while(left<right){
         mid = (left + right)/2;
         if(nums[mid]>mid) right = mid;
