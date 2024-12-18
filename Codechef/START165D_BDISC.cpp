e8f82a0041fdb1862b9088d310ea5dd9490d4925
Piyush <ultrapiyushkandwal@gmail.com>
2024-12-18 22:02:40 +0530

    bdisc

---
+++ Codechef/START165D_BDISC.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto &it : a)
+        cin >> it;
+        
+    int cost = 0;
+    sort(a.begin(), a.end());
+    cost += a[0];
+    int t = 1;
+    for(int i = 1 ; i < n ; ++i){
+        cost += max(0, a[i] - t);
+        t++;
+    }
+    cout << cost << '\n';
+    
+}
+
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin >> t;
+	while(t--){
+	    solve();
+	}
+
+}
