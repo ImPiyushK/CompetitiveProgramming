2e184f30ae1e04f837b321e1e58352d4a8a0729d
Piyush <ultrapiyushkandwal@gmail.com>
2024-12-18 22:03:26 +0530

    STABARR

---
+++ Codechef/START165D_STABARR.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto &it : a)
+        cin >> it;
+    
+    int count = 0, st = a[n - 1], ans = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (a[i] >= st)
+            {
+                st = a[i];
+                ans = max(ans, count);
+                count=0;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        ans = max(ans, count);
+
+        cout << ans << "\n";
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    solve();
+	}
+
+}
