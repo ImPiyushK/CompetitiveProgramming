ca7049cf657baa112a15c59d438074d6bb5288dd
Piyush <ultrapiyushkandwal@gmail.com>
2024-12-18 15:56:37 +0530

    leetcode

---
+++ Leetcode/1475.Final-Prices-With-A-Special-Discount-In-A-Shop.cpp
@@ -0,0 +1,41 @@
+//1. 2 for loop
+//2. Monotonic stack
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+
+        for(int i = 0 ; i < n ; ++i){
+            for(int j = i + 1 ; j < n ; ++j){
+                if(prices[j] <= prices[i]){
+                   prices[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+};
+
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        // Create a copy of prices array to store discounted prices
+        vector<int> result = prices;
+
+        stack<int> stack;
+
+        for (int i = 0; i < prices.size(); i++) {
+            // Process items that can be discounted by current price
+            while (!stack.empty() && prices[stack.top()] >= prices[i]) {
+                // Apply discount to previous item using current price
+                result[stack.top()] -= prices[i];
+                stack.pop();
+            }
+            // Add current index to stack
+            stack.push(i);
+        }
+
+        return result;
+    }
+};
