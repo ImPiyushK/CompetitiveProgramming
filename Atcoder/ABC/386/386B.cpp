61a981f74adc9f1521aadfc03566fa9ca41e1b5b
Piyush <ultrapiyushkandwal@gmail.com>
2024-12-28 18:26:31 +0530

    386B

---
+++ Atcoder/ABC/386/386B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    string s;
+    cin >> s;
+
+    int c = 0;
+
+    for(int i = 0 ; i < s.size() ; ++i){
+        if(s[i] == '1' || s[i] == '2' || s[i] == '3' || s[i] == '4' || s[i] == '5' || s[i] == '6' || s[i] == '7' || s[i] == '8' || s[i] == '9')
+            c++;
+        else if(s[i] == '0'){
+            if(i != s.size() - 1 && s[i + 1] == '0')
+                i++;
+            c++;
+        }
+    }
+    cout << c;
+    return 0;
+}
