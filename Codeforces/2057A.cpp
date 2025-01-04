b23a07994ea1221853331e689eccca03a0a0b54f
Piyush <ultrapiyushkandwal@gmail.com>
2025-01-04 20:38:28 +0530

    2057A

---
+++ Codeforces/2057A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n, m;
+    cin >> n >> m;
+
+    cout << max(n, m) + 1 << '\n';
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
