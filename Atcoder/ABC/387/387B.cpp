b22cedbcd10a38430abfaf117fb0aaf001d4e1c9
Piyush <ultrapiyushkandwal@gmail.com>
2025-01-05 16:20:08 +0530

    387B

--- Atcoder/ABC/387/387B.cpp
+++ Atcoder/ABC/387/387B.cpp
@@ -5,14 +5,12 @@ int main() {
     int x;
     cin >> x;
 
-    vector<vector<int>> grid(10, vector<int>(10));
     int sum = 0;
-
     for(int i = 1 ; i <= 9 ; ++i){
         for(int j = 1 ; j <= 9 ; ++j){
-            grid[i][j] = i * j;
-            if(x != grid[i][j])
-                sum += grid[i][j];
+
+            if(x != i*j)
+                sum += i * j;
         }
     }
     cout << sum;
