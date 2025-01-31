d721766bcb35e50c8e0861c43a428c7f91a0f882
Piyush <ultrapiyushkandwal@gmail.com>
2025-01-31 16:26:52 +0530

    Create ADVITIYA.cpp

---
+++ Codechef/START171/ADVITIYA.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    string s;
+	    cin >> s;
+	    
+	    string og = "ADVITIYA";
+	    if(og == s){
+	        cout << 0 << '\n';
+	        continue;
+	    }
+	    int c = 0;
+	    for(int i = 0 ; i < s.size() ; ++i){
+	        if(s[i] != og[i]){
+	           c += (og[i] - s[i] + 26) % 26;
+	        }
+	    }
+	    cout << c << '\n';
+	}
+
+}
+
+//  if(og[i] < s[i]){
+// 	                c += ('Z' - s[i]) + ('A' - og[i]);
+// 	            }
+// 	            else
+// 	                c += og[i] - s[i];
