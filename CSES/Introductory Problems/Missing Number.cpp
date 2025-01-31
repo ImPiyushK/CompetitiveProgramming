2496e707e879f8424e99d6c7540de1242b48f473
Piyush <ultrapiyushkandwal@gmail.com>
2025-01-31 17:12:39 +0530

    Create Missing Number.cpp

---
+++ CSES/Introductory Problems/Missing Number.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+
+    ll n;
+    cin >> n;
+    vector<ll> ar(n-1);
+    ll sum = 0, t;
+    for(ll i = 0 ; i < n - 1 ; ++i){
+        cin >> t;
+        sum += t;
+    }
+    ll tsum = n * (n +1 ) / 2;
+    cout << tsum - sum;    
+
+    return 0;
+}
