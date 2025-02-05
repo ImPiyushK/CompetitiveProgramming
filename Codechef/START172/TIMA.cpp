6032ff522c9fb9d0d4746d016e948501457cb630
Piyush <ultrapiyushkandwal@gmail.com>
2025-02-05 20:04:31 +0530

    Create TIMA

---
+++ Codechef/START172/TIMA.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+
+    if(x + 25 >= 2050)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
