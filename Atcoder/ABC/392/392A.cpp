c2c583f08c181443639ac81f372aca3f1e7aa9bd
Piyush <ultrapiyushkandwal@gmail.com>
2025-02-08 19:29:33 +0530

    392A

--- Atcoder/ABC/392/392A.cpp
+++ Atcoder/ABC/392/392A.cpp
@@ -1,16 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int a1, a2, a3;
-    cin >> a1 >> a2 >> a3;
+// int main(){
+//     int a1, a2, a3;
+//     cin >> a1 >> a2 >> a3;
 
-    if((a1 * a2) == a3)
+//     if((a1 * a2) == a3)
+//         cout << "Yes";
+//     else if((a1 * a3) == a2)
+//         cout << "Yes";
+//     else if((a2 * a3) == a1)
+//         cout << "Yes";
+//     else
+//         cout << "No";
+// }
+
+//OR
+
+int main(){
+    vector<int> a(3);
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a.begin(), a.end());
+
+    if(a[0]*a[1] == a[2])
         cout << "Yes";
-    else if((a1 * a3) == a2)
-        cout << "Yes";
-    else if((a2 * a3) == a1)
-        cout << "Yes";
-    else
+    else 
         cout << "No";
 }
