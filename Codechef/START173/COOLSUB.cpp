2dd103f889d4fe832cef328a5b411f853db543b2
Piyush <ultrapiyushkandwal@gmail.com>
2025-02-12 20:25:12 +0530

    COOLSUB

---
+++ Codechef/START173/COOLSUB.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> getFirstRepeatingElement(const vector<int>& arr) {
+    unordered_map<int, int> frequency;
+    
+    for (size_t i = 0; i < arr.size(); ++i) {
+        if (++frequency[arr[i]] > 1) {
+            return {arr[i]};
+        }
+    }
+    return {};
+}
+
+void processTest() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    vector<int> result = getFirstRepeatingElement(arr);
+
+    if (!result.empty()) {
+        cout << result.size() << endl;
+        for (int num : result) {
+            cout << num << " ";
+        }
+        cout << '\n';
+    } else {
+        cout << -1 << endl;
+    }
+}
+
+int main() {
+    int testCases;
+    cin >> testCases;
+    
+    while (testCases--) {
+        processTest();
+    }
+
+    return 0;
+}
