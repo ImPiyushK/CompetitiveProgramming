fa26b7f8d81fc2f639a1a24b256637df99e810d0
Piyush <ultrapiyushkandwal@gmail.com>
2025-02-12 20:24:22 +0530

    WAPEN

--- Codechef/START173/WAPEN.cpp
+++ Codechef/START173/WAPEN.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
+
+	#ifndef ONLINE_JUDGE
+    freopen("E:/Workspaces/CompetitiveProgramming/input.in", "r", stdin);
+    freopen("E:/Workspaces/CompetitiveProgramming/output.in", "w", stdout);
+    #endif
+
 	int x, y;
 	cin >> x >> y;
 	
