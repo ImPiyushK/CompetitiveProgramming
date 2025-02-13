56d1211bea8febd5372ab2878767443c3b7ef59c
Piyush <ultrapiyushkandwal@gmail.com>
2025-02-13 09:20:22 +0530

    MINOVER

--- Codechef/START173/MINOVER.cpp
+++ Codechef/START173/MINOVER.cpp
@@ -1,13 +1,53 @@
-#include <bits/stdc++.h>
+// #include <bits/allocator.h>
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include "bits/stdc++.h"
 using namespace std;
-
-int main() {
-
-	#ifndef ONLINE_JUDGE
-    freopen("E:/Workspaces/CompetitiveProgramming/input.in", "r", stdin);
-    freopen("E:/Workspaces/CompetitiveProgramming/output.in", "w", stdout);
-    #endif
-
-	
-
+using ll = long long int;
+ 
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
+ 
+    int t; cin >> t;
+    while (t--) {
+        int n, m; cin >> n >> m;
+        vector<int> a(n), b(m);
+        for (int &x : a) cin >> x;
+        for (int &x : b) cin >> x;
+ 
+        // rotate(begin(b), ranges::min_element(b), end(b)); // Bring the smallest element of b to the front
+        rotate(b.begin(), min_element(b.begin(), b.end()), b.end());
+        if (m == 1) {
+            for (int i = 0; i < n; ++i) cout << min(a[i], b[0]) << ' ';
+            cout << '\n';
+            continue;
+        }
+        
+        for (int i = 0; i+m <= n; ++i) {
+            if (a[i] < b[0]) continue;
+ 
+            bool replace = true;
+            if (a[i] == b[0]) {
+                replace = false;
+                for (int j = 0; j < m; ++j) {
+                    if (a[i+j] == b[j]) continue;
+                    if (a[i+j] < b[j]) break;
+                    replace = true;
+                    break;
+                }
+            }
+ 
+            if (!replace) continue;
+ 
+            for (int j = i; j < n; ++j)
+                a[j] = b[0];
+            for (int j = 0; j < m; ++j)
+                a[n-1-j] = b[m-1-j];
+            break;
+        }
+ 
+        for (int x : a) cout << x << ' ';
+        cout << '\n';
+    }
 }
