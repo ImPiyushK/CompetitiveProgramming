2c3da8f1a6df9930257092aae007fbd34ecbae8b
Piyush <ultrapiyushkandwal@gmail.com>
2025-02-13 09:11:26 +0530

    COOLSUB

---
+++ Codechef/START173/MINOVER.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+    freopen("E:/Workspaces/CompetitiveProgramming/input.in", "r", stdin);
+    freopen("E:/Workspaces/CompetitiveProgramming/output.in", "w", stdout);
+    #endif
+
+	
+
+}
