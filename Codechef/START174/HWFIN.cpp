9555226722b214ce65d0001b636ffd9df62bfd4f
Piyush <ultrapiyushkandwal@gmail.com>
2025-02-19 20:32:12 +0530

    HWFIN

---
+++ Codechef/START174/HWFIN.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	
+	if(x + 10*y >= 100)
+	    cout << "YES\n";
+	else
+	    cout << "NO\n";
+	    
+	return 0;
+
+}
