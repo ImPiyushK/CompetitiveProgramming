60d1c41444249988306340c72fae374fb843c315
Piyush <32041731+ImPiyushK@users.noreply.github.com>
2025-02-27 18:51:40 +0530

    Update 3465. Find Products with Valid Serial Numbers.cpp

--- Leetcode/3465. Find Products with Valid Serial Numbers.cpp
+++ Leetcode/3465. Find Products with Valid Serial Numbers.cpp
@@ -1,3 +1,2 @@
 # Write your MySQL query statement below
-SELECT * FROM products WHERE description REGEXP "SN[0-9]{4}-[0-9]{4}$" 
-OR description REGEXP "SN[0-9]{4}-[0-9]{4}[^0-9]+"ORDER BY product_id
+SELECT * FROM products WHERE description REGEXP "SN[0-9]{4}-[0-9]{4}$" OR description REGEXP "SN[0-9]{4}-[0-9]{4}[^0-9]+"ORDER BY product_id
