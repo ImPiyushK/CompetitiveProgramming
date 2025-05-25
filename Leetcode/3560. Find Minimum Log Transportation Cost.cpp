87b3786303002fca5676cb413b0f49c9582aacf4
Piyush <ultrapiyushkandwal@gmail.com>
2025-05-25 22:10:49 +0530

    3560

--- Leetcode/3560. Find Minimum Log Transportation Cost.cpp
+++ Leetcode/3560. Find Minimum Log Transportation Cost.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     long long minCuttingCost(int n, int m, int k) {
         long long cost = 0;
-        while(n > k){
-            cost += (long long)k * (long long)(n-k);
-            n -= k;
-        }
-        while(m > k){
-            cost += (long long)k * (long long)(m-k);
-            m -= k;
-        }
+        
+        if (n > k) 
+            cost += (long long)k * (long long)(n - k);
+        if (m > k) 
+            cost += (long long)k * (long long)(m - k);
+        
         return cost;
     }
 };
