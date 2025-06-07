80b49898d4dce9cf0e80dcd3739be60062ac0bc9
Piyush <ultrapiyushkandwal@gmail.com>
2025-06-07 17:41:54 +0530

    409A

---
+++ Atcoder/ABC/409/409A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ll long long
+#define line cout << '\n'
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define p(ans) cout<<(ans)<<'\n'
+#define f first
+#define s second
+
+#define PI 3.1415926535897932384626433832795l
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+void solve() {
+    int n;
+    cin >> n;
+    string t, a;
+    cin >> t >> a;
+
+    for(int i = 0 ; i < n ; ++i){
+        if(t[i] == 'o' && t[i] == 'o'){
+            cout << "Yes\n";
+            return;
+        }
+    }
+    cout << "No\n";
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+    freopen("output.in", "w", stdout);
+    #endif
+
+    fastio
+
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+    return 0;
+}
