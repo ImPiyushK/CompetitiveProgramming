7831c1e69e1f144908ec59fddc908406fb5feb94
Piyush <ultrapiyushkandwal@gmail.com>
2025-07-26 16:28:40 +0530

    Insertion SLL

---
+++ LinkedList/2.InsertionSLL.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* insertionBeginning(Node* head, int n){
+    if(head == nullptr){
+        head = new Node(n);
+    }
+    else{
+        Node *temp = new Node(n);
+        temp->next = head;
+        head = temp;
+    }
+    return head;
+}
+
+void traverse(Node *head){
+    cout << "Linked List: ";
+    while(head != nullptr){     //while(head)
+        cout << head->data << ' ';
+        head = head->next;
+    }
+}
+
+int main(){
+    Node *head = nullptr;
+    int n = 10;
+    int arr[] = {2, 4, 6, 8, 10};
+    for(int i = 0 ; i < 5 ; ++i){
+        head = insertionBeginning(head, arr[i]);
+    }
+    traverse(head);
+}
