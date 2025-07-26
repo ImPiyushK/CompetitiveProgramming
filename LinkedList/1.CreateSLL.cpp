2f246c5fb09b919fa25e66008805cf526c05e2cb
Piyush <ultrapiyushkandwal@gmail.com>
2025-07-26 16:08:36 +0530

    Create SLL

---
+++ LinkedList/1.CreateSLL.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    public:
+    Node(){
+        data = 0;
+        next = nullptr;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+};
+
+int main(){
+    Node* head = new Node(1);
+    Node* p = new Node(2, nullptr);
+    head->next = p;
+
+    cout << head << ' ';
+    cout << head->data << ' ';
+    cout << head->next << '\n';
+
+    cout << head->next << ' ' << head->next->data << ' ' << head->next->next;    
+}
