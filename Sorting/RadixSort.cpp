cf2bcf990595211f039d4504bcf4724912af5d5a
Piyush <ultrapiyushkandwal@gmail.com>
2025-07-28 19:15:46 +0530

    Radix Sort

--- Sorting/RadixSort.cpp
+++ Sorting/RadixSort.cpp
@@ -18,15 +18,19 @@ void countingSort(vector<int>& arr, int exp) {
         count[i] += count[i - 1];
 
     for (int i = n - 1; i >= 0; i--) {
-        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-        count[(arr[i] / exp) % 10]--;
+        int digit = (arr[i] / exp) % 10;
+        output[--count[digit]] = arr[i];
     }
+    //OR Upper Method is more efficient
+    // for (int i = n - 1; i >= 0; i--) {
+    //     output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+    //     count[(arr[i] / exp) % 10]--;
+    // }
 
     // Copy the output array to arr[],
     // so that arr[] now contains sorted
     // numbers according to current digit
-    for (i = 0; i < n; i++)
-        arr[i] = output[i];
+    arr = output;
     
 }
 
