6f5600bf1da36e292e6424f915c72f74a5fb38c0
Piyush <ultrapiyushkandwal@gmail.com>
2025-07-28 18:33:34 +0530

    Counting Sort

---
+++ Sorting/CountingSort.cpp
@@ -0,0 +1,48 @@
+ #include<bits/stdc++.h>
+using namespace std;
+
+vector<int> countingSort(vector<int> arr, int n){
+
+    //if array empty
+    if (n == 0)     
+        return {};
+    
+    int n = arr.size();
+
+    // find the maximum element
+    int maxval = arr[0];
+    for (int i = 1; i < n; i++)
+        maxval = max(maxval, arr[i]);
+
+    // create and initialize count array
+    vector<int> count(maxval + 1, 0);
+
+    // count frequency of each element
+    for (int i = 0; i < n; i++)
+        count[arr[i]]++;
+
+    // compute prefix sum
+    for (int i = 1; i <= maxval; i++)
+        count[i] += count[i - 1];
+
+    // build output array
+    vector<int> ans(n);
+    for (int i = n - 1; i >= 0; i--) {
+        ans[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {2, 5, 3, 0, 2, 3, 0, 3};
+    int n = sizeof(arr)/sizeof(arr[0]); 
+
+    vector<int> ans = countingSort(arr, n);
+
+    cout << "Sorted array: \n";
+    for (int num : ans)
+        cout << " " << num;
+    cout << '\n';
+}
