0ffbd86e9f0ac260b57133d06db9900be951bc95
Piyush <ultrapiyushkandwal@gmail.com>
2025-07-29 21:47:12 +0530

    2094A

---
+++ Codeforces/2000-2100/2094A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ll long long
+#define line cout << '\n'
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define p(ans) cout<<(ans)<<'\n'
+#define f first
+#define s second
+
+#define PI 3.1415926535897932384626433832795l
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+void solve() {
+	string a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a[0] << b[0] << c[0] << '\n';
+}
+
+int main() {
+
+	fastio
+
+	int tc = 1;
+	cin >> tc;
+	for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+    return 0;
+}
