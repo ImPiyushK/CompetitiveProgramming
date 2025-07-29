32b95d4185cabb39a3c4819832e782475356ca1e
Piyush <ultrapiyushkandwal@gmail.com>
2025-07-29 21:46:00 +0530

    Insertion Beginning Recursion SLL

---
+++ LinkedList/2d.InsertionBeginningRecursionSLL.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node * createLinkedListStart(int arr[], int index, int n){
+    
+}
+
+void traverse(Node *head){
+    Node* t = head;
+    cout << "Linked List: ";
+    while(t != nullptr){     //while(head)
+        cout << t->data << ' ';
+        t = t->next;
+    }
+}
+
+int main(){
+    Node* head = nullptr;
+    int arr[] = {2,4,6,7,8,9};
+
+    head = createLinkedListStart(arr, 0, 6);
+    traverse(head);
+}
