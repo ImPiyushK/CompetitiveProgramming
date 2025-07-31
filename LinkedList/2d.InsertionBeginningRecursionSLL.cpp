2feacdb82802224f24f5693923db8b9b621827fe
Piyush <ultrapiyushkandwal@gmail.com>
2025-07-31 14:45:32 +0530

    Insertion Recursion Start SLL

--- LinkedList/2d.InsertionBeginningRecursionSLL.cpp
+++ LinkedList/2d.InsertionBeginningRecursionSLL.cpp
@@ -13,8 +13,14 @@ class Node{
     }
 };
 
-Node * createLinkedListStart(int arr[], int index, int n){
-    
+Node * createLinkedListStart(int arr[], int index, int n, Node *prev){
+    if(index == n)
+        return prev;
+
+    Node* temp = new Node(arr[index]);
+    temp->next = prev;
+
+    return createLinkedListStart(arr, index + 1, n, temp);
 }
 
 void traverse(Node *head){
@@ -30,6 +36,6 @@ int main(){
     Node* head = nullptr;
     int arr[] = {2,4,6,7,8,9};
 
-    head = createLinkedListStart(arr, 0, 6);
+    head = createLinkedListStart(arr, 0, 6, head);
     traverse(head);
 }
