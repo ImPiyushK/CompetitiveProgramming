c295a644d5fbc2f61bbe9b277e77614c7a4b663e
Piyush <ultrapiyushkandwal@gmail.com>
2025-07-31 18:31:58 +0530

    Insertion

---
+++ LinkedList/2e.InsertionParticularPositionSLL copy.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* createList(Node* head, int n){
+    if(head == nullptr){
+        head = new Node(n);
+    }
+    else{
+        Node *temp = new Node(n);
+        temp->next = head;
+        head = temp;
+    }
+    return head;
+}
+
+void traverse(Node *head){
+    Node* t = head;
+    cout << "Linked List: ";
+    while(t != nullptr){     //while(head)
+        cout << t->data << ' ';
+        t = t->next;
+    }
+}
+
+Node* insertMiddle(Node* head, int x, int key){
+    Node *temp = head;
+    x -= 1;
+    while(x--){
+        temp = temp->next;
+    }
+    Node *p = new Node(key);
+    p->next = temp->next;
+    temp->next = p;
+
+    return head;
+}
+
+int main(){
+    Node *head = nullptr;
+    int n = 10;
+    int arr[] = {2, 4, 6, 8, 10};
+    for(int i = 0 ; i < 5 ; ++i){
+        head = createList(head, arr[i]);
+    }
+    // Insert element afther the 3rd(x) position
+    int x = 3;
+    int key = 5;
+    head = insertMiddle(head, x, key);
+    traverse(head);
+}
