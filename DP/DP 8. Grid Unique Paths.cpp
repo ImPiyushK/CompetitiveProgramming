baa873ee2618c9bbc891bdea8fda712037a9ff4d
Piyush <ultrapiyushkandwal@gmail.com>
2025-11-13 01:44:50 +0530

    DP8

--- DP/DP 8. Grid Unique Paths.cpp
+++ DP/DP 8. Grid Unique Paths.cpp
@@ -12,3 +12,21 @@ public:
         return uniquePathHelper(m-1, n-1);  //m, n is grid position not indices
     }
 };
+
+class Solution {
+public:
+    int uniquePathHelper(int m, int n, vector<vector<int>> &dp){
+        if(m == 0 && n == 0)
+            return 1;
+        if(m < 0 || n < 0)
+            return 0;
+        if(dp[m][n] != -1)
+            return dp[m][n];
+        dp[m][n] = uniquePathHelper(m-1, n, dp) + uniquePathHelper(m, n-1, dp);
+        return dp[m][n];
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return uniquePathHelper(m-1, n-1, dp);  //m, n is grid position not indices
+    }
+};
