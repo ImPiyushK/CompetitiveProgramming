698e097bd63afd9e1dcf86c73baf663bf1aa948b
Piyush <ultrapiyushkandwal@gmail.com>
2025-11-15 18:40:11 +0530

    Update DP 10

--- DP/DP10. Minimum Path Sum In a Grid.cpp
+++ DP/DP10. Minimum Path Sum In a Grid.cpp
@@ -47,21 +47,6 @@ public:
 // Tabulation - Bottom Up
 class Solution {
 public:
-    int helperMinPathSum(vector<vector<int>>& grid, vector<vector<int>> &dp, int i, int j){
-        
-        if(i == 0 && j == 0)
-            return grid[0][0];
-        if(i < 0 || j < 0)
-            return INT_MAX;
-
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        int up = helperMinPathSum(grid, dp, i, j-1);
-        int left = helperMinPathSum(grid, dp, i-1, j);
-        return dp[i][j] = grid[i][j] + min(up, left);
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
